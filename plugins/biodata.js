a01f46d9c4b0c4a1f9aab8617497fa24f15de3ef
davidpangrib001 <96653161+davidpangrib001@users.noreply.github.com>
2022-03-28 19:16:38 +0700

    Update biodata.js

--- plugins/biodata.js
+++ plugins/biodata.js
@@ -3,25 +3,25 @@ let handler = async (m, { conn, text }) => {
   let ext= `
 ╭═════════════════
 ║╭──❉ *𝐁𝐈𝐎𝐃𝐀𝐓𝐀 𝐎𝐖𝐍𝐄𝐑* ❉───
-║│➸ *Nama* : LanXzy🤸
-║│➸ *Umur* : 15
-║│➸ *Kelas* :  9 SMP
+║│➸ *Nama* : David 🤸
+║│➸ *Umur* : 11
+║│➸ *Kelas* :  6 SD
 ║│➸ *Status* : "Day" is not important to me
 ║╰───────────────
 ╰════════════════
 ╭════════════════
 ║╭──❉ *𝐒𝐎𝐒𝐈𝐀𝐋 𝐌𝐄𝐃𝐈𝐀* ❉───
-║│➸ *No.Owner* : wa.me/60189830350
-║│➸ *No.Bot* : wa.me/60149431385
-║│➸ *Chanel Youtube* : https://youtube.com/channel/UC3Y7rHuI_OmbvFdL88JF7AQ
-║│➸ *Gmail* : Xnxx@gmail.com
-║│➸ *Github* : males isi
+║│➸ *No.Owner* : wa.me/6282160268332
+║│➸ *No.Bot* : wa.me/6282128475388
+║│➸ *Chanel Youtube* : NaN
+║│➸ *Gmail* : Private
+║│➸ *Github* : Private
 ║╰────────────────
 ╰═════════════════
 _Oke udah itu aja terimakasih_
 
 `.trim()
-conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'Raditya')).buffer(), ext, 'Recoded By Raditya', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewa', m)
+conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'Raditya')).buffer(), ext, 'Recoded By David', 'Nomor Owner', '.owner', 'Donasi', '.donasi', 'Sewa Bot', '.sewa', m)
 
 }
 handler.help = ['infoowner', 'infocreator']
