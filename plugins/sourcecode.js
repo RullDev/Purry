54e3f4e9c6f57e21bc6e48696f024bc58c09db92
davidpangrib001 <96653161+davidpangrib001@users.noreply.github.com>
2022-03-28 21:08:49 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,8 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Source code')).buffer(), `
-𝙎𝘾 𝙊𝙍𝙄 𝘽𝙔 𝙇𝙔𝙉𝙓𝙕𝙔 🔥
-github.com/LynnXzy/scbotz-v1
-`.trim(), '©LynXzy🥀', 'Makasih', 'Thanks')
+𝖧𝖺𝗁, 𝗌𝖼 ? 𝖼𝗈𝖻𝖺 𝗇𝗎𝗇𝗀𝗀𝗂𝗇𝗀 𝖺𝗃𝖺 𝖽𝖾𝖼𝗄 🐦
+`.trim(), 'Lihatlah dia, tahun ini masih ngemis sc wkwk..', 'Owner', '#owner')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
