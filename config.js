7a929496627b328347b20ffbd98759a349860adf
davidpangrib001 <96653161+davidpangrib001@users.noreply.github.com>
2022-03-30 11:21:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,100 +1,65 @@
-/*Bismillahirrahmanirrahim
- thank you to ALLAH Swt
-thank you to Nurutomo as wabot-aq
-thank you to ariffb as stikerinbot
-thank you to botstylee
- thank you to bochilgaming as games-wabot
-thank you to benni ismael
-thank you to zerochanBot
-thank you to LynnXzy
-thank you to MikeBot Dev Team
- thank you to ALL Bot creator
- and thanks you to who support my Bot
- */
-let fs = require('fs')
-let chalk = require('chalk')
+// Thanks For Allah
+// AlyaaXzy
+// BOTCAHX
 
-global.linkGC = ['https://chat.whatsapp.com/EVCGfzxLWfp81n0WhmVklH', 'https://chat.whatsapp.com/FnNAbem8o6r4pgLhSdO8Q9', 'https://chat.whatsapp.com/HBhy7rVae3o0PkIfkhvs2N'] // ganti jadi group lu
-global.owner = ['6282160268332', '0', '6282128475388'] // Masukan nomot kalian
-global.mods = ['6282160268332'] // Moderator
-global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Pengguna premium tidak memerlukan limit
+
+let fs = require('fs')
+global.owner = ['6282160268332', '6282128475388','0'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
-  // name: 'https://website'
-  bx: 'https://bx-hunter.herokuapp.com',
+  // nama: 'https://website'
+  hardianto: 'https://hardianto-chan.herokuapp.com',
   rey: 'https://server-api-rey.herokuapp.com',
-  hardianto: 'https://hardianto.xyz',
+  jonaz: 'https://jonaz-api-v2.herokuapp.com',
   neoxr: 'https://neoxr-api.herokuapp.com',
   nrtm: 'https://nurutomo.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz',
   xteam: 'https://api.xteam.xyz',
   zahir: 'https://zahirr-web.herokuapp.com',
-  lol: 'https://api.lolhuman.xyz',
-  caliph: 'https://caliphapi.com',
-  dhnjing: 'https://dhnjing.xyz',
-  zeks: 'https://api.zeks.me',
-  pencarikode: 'https://pencarikode.xyz',
+  zekais: 'http://zekais-api.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  vhtear: 'https://api.vhtear.com',
+  lolhum: 'https://api.lolhuman.xyz',
   fxc7: 'https://fxc7-api.herokuapp.com',
-  LeysCoder: 'https://leyscoders-api.herokuapp.com'
+  bx: 'https://bx-hunter.herokuapp.com',
+
 }
-global.APIKeys = { // APIKey Here
-  // 'https://website': 'apikey' (apikey kalian^_^)
+global.APIKeys = { // APIKey nya disini
+  // 'https://website': 'apikey'
   'https://server-api-rey.herokuapp.com': 'apirey',
-  'https://bx-hunter.herokuapp.com': 'Ikyy69',
-  'https://hardianto.xyz': 'hardianto',
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
-  'https://api.xteam.xyz': 'cristian9407',
-  'https://api.lolhuman.xyz': 'Dawnfrostkey',
-  'https://caliphapi.com': 'ELYASXD',
-  'https://zahirr-web.herokuapp.com': 'zahirgans',
-  'https://api.zeks.me': 'apivinz',
   'https://pencarikode.xyz': 'pais',
-  'https://fxc7-api.herokuapp.com': 'fxc7COOL',
-  'https://leyscoders-api.herokuapp.com': 'dappakntlll'
+  'https://api.xteam.xyz': 'HIRO',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'nyyxz-bot',
+  'https://api.lolhuman.xyz': 'ed6c4f2c72b6f92f7d201464',
+  'https://api.vhtear.com': 'sayahafiz',
+  'https://fxc7-api.herokuapp.com': 'uN8rsK4g',
+  'https://api.justaqul.xyz': '5kbUqdG00OXelFYx',
+  'http://zekais-api.herokuapp.com': 'grqgD6pU',
+  'https://bx-hunter.herokuapp.com': 'Ikyy69',
 }
 
 // Sticker WM
-const spack = fs.readFileSync("lib/exif.json")
-const stickerpack = JSON.parse(spack)
-if (stickerpack.spackname == '') {
-  var sticker_name = 'WhatsApp'
-  var sticker_author = '2.22.3.77'
-} else {
-  var sticker_name = stickerpack.spackname
-  var sticker_author = stickerpack.sauthor
-}
+global.packname = 'WhatsApp'
+global.author = '2.22.3.77'
 
-const file_exif = "lib/exif.json"
-fs.watchFile(file_exif, () => {
-  fs.unwatchFile(file_exif)
-  console.log(chalk.redBright("Update 'exif.json'"))
-  delete require.cache[file_exif]
-  require('./lib/exif.json')
-})
+global.wait = '*「 ⏱️ 」Harap tunggu...*'
+global.eror = '*「❗」Terjadi Kesalahan*'
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning&text='
+global.watermark = '𝗢𝗻𝗲 𝗕𝗼𝘁 𝗯𝘆 𝗗𝗮𝘃𝗶𝗱' //change the watermark 
+global.image = 'https://telegra.ph/file/8862875cbcfdfe32e5103.jpg' //change the image
+global.thumbfoto = 'https://telegra.ph/file/8862875cbcfdfe32e5103.jpg'
 
-global.packname = sticker_name
-global.author = sticker_author
-
-// silakan di ganti klo mau
-global.wait = '*▰▰▰▰▱▱▱▱▱▱ ʟᴏᴀᴅɪɴɢ...*'
-global.rpg = 'Fitur Rpg Dimatikan\nKetik *#enable* *rpg* untuk menggunakan fitur ini'
-global.nsfw = '“Katakanlah kepada orang laki-laki yang beriman: Hendaklah mereka menahan pandanganya, dan memelihara kemaluannya; … Katakanlah kepada wanita yang beriman: Hendaklah mereka menahan pandangannya, dan kemaluannya, dan janganlah mereka Menampakkan perhiasannya, kecuali yang (biasa) nampak dari padany,” \n(TQS. Al-Nur [24]: 30-31).'
-global.eror = '_*Terjadi Kesalahan*_'
-
-global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
-global.watermark = '\n𝗢𝗻𝗲 𝗕𝗼𝘁 𝗯𝘆 𝗗𝗮𝘃𝗶𝗱\n' //change the watermark (beberapa harus manual)
-
-// Ubah saja
-global.image = 'https://telegra.ph/file/8862875cbcfdfe32e5103.jpg'//thumbnail
+// image
 global.bank = 'https://telegra.ph/file/d5ddf4cc627bb0e6bc420.jpg'
 global.kandang = 'https://telegra.ph/file/67a6ee607d03a4e52757d.jpg'
 global.kolam = 'https://telegra.ph/file/5aa5dfa3394477e11fb18.jpg'
 global.thanks = 'https://telegra.ph/file/f9c55dea5728df6345670.jpg'
 
-// tingkat kesulitan, semakin tinggi semakin susah
-global.multiplier = 500 // The higher, The harder levelup
+global.multiplier = 2134 // Semakin tinggi, semakin sulit naik level
 
-//*****************PEMBATAS*********************
-// JANGAN DI GANTI NTAR KLO GABISA JAN TANYA GW
-
+let chalk = require('chalk')
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
   fs.unwatchFile(file)
