2d3a1702d15be273c239f11b5b55f59daef9b42e
davidpangrib001 <96653161+davidpangrib001@users.noreply.github.com>
2022-03-31 10:21:20 +0700

    Update Ftroli.js

--- plugins/Ftroli.js
+++ plugins/Ftroli.js
@@ -2,10 +2,10 @@ let fs = require('fs')
 let handler  = async (m, { conn, usedPrefix }) => {
 prep = conn.prepareMessageFromContent(m.chat, { orderMessage: { 
 itemCount: 999999999999, status: 1,
-message: `AwogAwogAwog`,
+message: `Pesanan Datang!`,
 orderTitle: 'B',
 sellerJid: '0@s.whatsapp.net',
-thumbnail: fs.readFileSync('./src/RadBotZ.jpg')
+thumbnail: fs.readFileSync('./src/One.jpg')
 }}, {contextInfo: null, quoted: m})
 conn.relayWAMessage(prep)
 }
